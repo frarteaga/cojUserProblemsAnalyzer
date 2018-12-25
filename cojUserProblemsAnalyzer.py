be13c0e9c6e7bafd26f51d4b616738eb819eeb78
farteaga8912 <18072082+frarteaga@users.noreply.github.com>
2018-12-25 18:00:30 -0500

    Comment for purpose of the script

---
+++ cojUserProblemsAnalyzer.py
@@ -0,0 +1,19 @@
+"""
+COJ (http://coj.uci.cu) User Problems Analyzer
+
+Inspect several aspects of a given a list of COJ users.
+
+The list of usernames are given from the command line.
+
+Author: frankr@coj
+"""
+
+import urllib
+import sys
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
