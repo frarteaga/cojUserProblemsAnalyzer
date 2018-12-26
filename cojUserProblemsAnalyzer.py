d7a81e518f61ea9274b0f35d2adc03f104c4f57c
farteaga8912 <18072082+frarteaga@users.noreply.github.com>
2018-12-26 10:46:47 -0500

    Extract problems list using an html parser

--- cojUserProblemsAnalyzer.py
+++ cojUserProblemsAnalyzer.py
@@ -12,9 +12,46 @@ Author: frankr@coj
 
 import urllib
 import sys
+from HTMLParser import HTMLParser
 
 
 HTMLProfiles = {}
+ACCProblems = {}
+AttemptedProblems = {}
+
+
+class CojUserProfileParser(HTMLParser):
+    accScope = False
+    attemptedScope = False
+    pickProbleId = False
+    ACCProblems = []
+    AttemptedProblems = []
+
+    def handle_starttag(self, tag, attrs):
+        self.pickProbleId = False
+        if ('id', 'probsACC') in attrs:
+            self.accScope = True
+        elif ('id', 'probsWA') in attrs:
+            self.accScope = False
+            self.attemptedScope = True
+        elif tag == 'span' and (('class', 'badge alert-success') in attrs or ('class', 'badge alert-danger') in attrs):
+            self.pickProbleId = True
+
+    def is_valid_pid(self, data):
+        try:
+            pid = int(data)
+            if 1000 <= pid <= 9999:
+                return True
+            return False
+        except:
+            return False
+
+    def handle_data(self, data):
+        if self.pickProbleId and self.is_valid_pid(data):
+            if self.accScope:
+                self.ACCProblems.append(data)
+            elif self.attemptedScope:
+                self.AttemptedProblems.append(data)
 
 
 def GetUsernameList():
@@ -67,7 +104,19 @@ def DownloadUserProfiles(usernames):
         if HasProfileHtmlErrors(html):
             WriteLine('An error occured with ' + profileHtmlFileName + 
                   '. Make sure the user exist or check you internet connection')
+        else:
+            HTMLProfiles[username] = html
 
+
+def ExtractProblemListsFrom(htmlProfiles):
+    for username in htmlProfiles:
+        html = htmlProfiles[username]
+        parser = CojUserProfileParser()
+        parser.feed(html)
+        ACCProblems[username] = set(parser.ACCProblems)
+        AttemptedProblems[username] = set(parser.AttemptedProblems)
+
+        
 
 def main():
     usernames = GetUsernameList()
@@ -75,6 +124,7 @@ def main():
         ExitWithMessage("No username provided", 1)
 
     DownloadUserProfiles(usernames)
+    ExtractProblemListsFrom(HTMLProfiles)
 
 
 if __name__ == '__main__':
